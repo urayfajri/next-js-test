004ebc6ebe39d8059eb63daa24d3209bec801a68
Uray Muhamad Noor Fajri Widiansyah <77041388+urayfajri@users.noreply.github.com>
2025-02-28 05:37:47 +0700

    fix: reload page auth (#3)
    Co-authored-by: WDTI0\uray.widiansyah <uray.widiansyah@primecash.co.id>

--- src/app/providers/store/useAuthorizationStore/index.tsx
+++ src/app/providers/store/useAuthorizationStore/index.tsx
@@ -9,6 +9,7 @@ import { User } from '@/app/types/model/user/type';
 type AuthorizationStoreType = {
   user: User | null;
   isAuthenticated: boolean;
+  setIsAuthenticated: (isAuthenticated: boolean) => void;
   isLoading: boolean;
   login: (user: User) => void;
   logout: () => void;
@@ -57,6 +58,13 @@ const useAuthorizationStore = create<AuthorizationStoreType>((set) => ({
     set(
       produce<AuthorizationStoreType>((state) => {
         state.isLoading = false;
+      })
+    );
+  },
+  setIsAuthenticated(isAuthenticated: boolean) {
+    set(
+      produce<AuthorizationStoreType>((state) => {
+        state.isAuthenticated = isAuthenticated;
       })
     );
   },
